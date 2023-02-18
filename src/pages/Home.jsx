e6ef7efa882d86ad9127f59589dbe28d5661e2c3
Chris Mems <christopher.mems@.com>
2023-02-18 09:30:15 -0600

    style workout details component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import WorkoutDetails from '../components/WorkoutDetails'
 
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null)
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const res = await fetch('/api/workouts')
+      const json = await res.json()
+
+      if(res.ok) {
+        setWorkouts(json)
+      }
+    }
+
+    fetchWorkouts()
+  }, [])
+
   return (
     <div>
-      <h2>Home</h2>
+      <div className="workouts">
+        {workouts && workouts.map((workout) => (
+          <WorkoutDetails key={workout._id} workout={workout} />
+        ))}
+      </div>
     </div>
   )
 }
