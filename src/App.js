a36bc1d7805b8cf3a8f9e85db22aa01cc8816f97
Chris Mems <christopher.mems@.com>
2023-02-18 08:41:50 -0600

    style index.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-
+      <NavBar />
+      <div className="pages">
+        <Routes>
+          <Route
+            path='/'
+            element = {<Home />}
+          />
+        </Routes>
+      </div>
     </div>
   );
 }
